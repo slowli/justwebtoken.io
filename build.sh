456e71491c46d250cbf3e5c231272ef02483de5c
Alex Ostrovski <ostrovski.alex@gmail.com>
2021-11-06 15:34:21 +0200

    Configure Netlify

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Build script for one-shot build environments.
+
+set -ex
+
+if [[ "$RUST_VERSION" == "" ]]; then
+  echo "RUST_VERSION env var is not defined; set it to the Rust toolchain to use (e.g., 1.56.1)"
+  exit 1
+fi
+
+# Install Rust
+curl -sSf https://sh.rustup.rs | sh -s -- --default-toolchain="$RUST_VERSION" -y
+source ~/.cargo/env
+rustup target add --toolchain "$RUST_VERSION" wasm32-unknown-unknown
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
+rustup override set "$RUST_VERSION"
+
+# Build!
+PATH="$PATH:$HOME/.local/bin" BINARYEN_CORES=1 npm run build
